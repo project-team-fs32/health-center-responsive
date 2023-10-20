a33a19deac55708daff2be26903cfbee0097cca1
fajarriskihidayat <fajarrh2002@gmail.com>
2023-10-20 19:56:49 +0700

    fetch layanan by user_email

--- js/layanan.js
+++ js/layanan.js
@@ -1,14 +1,21 @@
 const ENDPOINT_LAYANAN = "https://652a44c14791d884f1fcb6a7.mockapi.io/layanan";
 const listData = document.getElementById("list-data");
 
+const data1 = localStorage.getItem("user");
+const user1 = JSON.parse(data1);
+
 const fetchLayanan = async () => {
   try {
     const res = await fetch(ENDPOINT_LAYANAN);
     const datas = await res.json();
 
-    datas.map((item, i) => {
+    let layanan = datas.filter((data) => {
+      return data.user_email === user1.email;
+    });
+
+    layanan.map((item, i) => {
       let list = `<tr>
-      <td>${item.id}</td>
+      <td>${i + 1}</td>
       <td>${item.nama_layanan}</td>
       <td>Rp ${item.biaya}</td>
       <td>${item.nama_dokter}</td>
@@ -53,6 +60,7 @@ simpan.addEventListener("click", () => {
       nama_dokter: dokter,
       jadwal: jadwal,
       image: foto,
+      user_email: user.email,
     };
 
     try {
